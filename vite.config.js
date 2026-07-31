6789918ea9dca141bc84dbbc1b1a0135630ef32b
YourGitHubghulamdata008-prog <your-ghulamdata008@gmail.com>
2026-07-31 21:22:45 +0500

    Fix missing student.css

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/css/student.css',
+
                 'resources/css/teacher.css',
                 'resources/js/app.js',
             ],
